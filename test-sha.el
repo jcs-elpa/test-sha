d432e388fc67dff7fd7e3b4d6e552191ed3e4602
jenchieh <jcs090218@gmail.com>
2020-07-26 22:32:04 +0800

    Add core.

---
+++ test-sha.el
@@ -0,0 +1,38 @@
+;;; test-sha.el --- Test ground for secure hash algorithms  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  Shen, Jen-Chieh
+;; Created date 2020-07-26 22:28:31
+
+;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; Description: Test ground for secure hash algorithms.
+;; Keyword: sha secure hash algorithm test
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "24.3"))
+;; URL: https://github.com/jcs090218/test-sha
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Test ground for secure hash algorithms.
+;;
+
+;;; Code:
+
+
+
+(provide 'test-sha)
+;;; test-sha.el ends here
